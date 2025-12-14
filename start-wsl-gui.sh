074ca0a5d8d0540a5dfea1ceb9dbcc0ee30f3010
kitty <kitty.eu.org@gmail.com>
2025-12-14 16:00:26 +0800

    update icon

--- start-wsl-gui.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# WSL GUI 环境变量设置
-export DISPLAY=$DISPLAY
-export LIBGL_ALWAYS_INDIRECT=1
-export GDK_BACKEND=wayland
-export QT_QPA_PLATFORM=wayland
-export NO_AT_BRIDGE=1
-# 启动应用
-exec "$@"
